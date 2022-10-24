e8093882ece3370a819c54d8f13cdf8ee7635285
Vinge Nikolay <vn2756@gmail.com>
2022-10-24 20:15:24 +0400

    SubstringFinder

---
+++ DiffTool.Test/Core/SubstringFinderTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DiffTool.Core;
+using NUnit.Framework;
+
+namespace DiffTool.Test.Core;
+
+internal class SubstringFinderTest
+{
+    private string _oldText;
+    private string _newText;
+    private List<SubstringResult> _result;
+    private SubstringFinder _finder;
+
+    [SetUp]
+    public void Setuo()
+    {
+        _finder = new SubstringFinder();
+    }
+
+    [Test]
+    public void GetResult_Empty()
+    {
+        _oldText = "";
+        _newText = "";
+
+        GetResult();
+
+        Assert.That(_result, Has.Count.EqualTo(0));
+    }
+
+    [Test]
+    public void GetResult_Same()
+    {
+        _oldText = "line 123 ABC";
+        _newText = "line 123 ABC";
+
+        GetResult();
+
+        Assert.That(_result, Has.Count.EqualTo(1));
+
+        Assert.That(_result[0], Is.EqualTo(new SubstringResult(0, 0, 12)));
+    }
+
+    [Test]
+    public void GetResult_1()
+    {
+        _oldText = "line 123 ABC";
+        _newText = "line ABC";
+
+        GetResult();
+
+        Assert.That(_result, Has.Count.EqualTo(2));
+
+        Assert.That(_result[0], Is.EqualTo(new SubstringResult(0, 0, 5)));
+        Assert.That(_result[1], Is.EqualTo(new SubstringResult(9, 5, 3)));
+    }
+
+    private void GetResult()
+    {
+        _result = _finder.GetResult(_oldText, _newText).ToList();
+    }
+}
---
+++ DiffTool/Core/SubstringFinder.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+
+namespace DiffTool.Core;
+
+internal readonly struct SubstringResult
+{
+    public readonly int OldPosition;
+    public readonly int NewPosition;
+    public readonly int Count;
+
+    public SubstringResult(int oldPosition, int newPosition, int count)
+    {
+        OldPosition = oldPosition;
+        NewPosition = newPosition;
+        Count = count;
+    }
+
+    public override string ToString()
+    {
+        return $"{OldPosition}:{NewPosition} {Count}";
+    }
+}
+
+internal class SubstringFinder
+{
+    private int[,] _searchMatrix;
+
+    public SubstringFinder()
+    {
+        _searchMatrix = new int[100, 100];
+    }
+
+    public IReadOnlyCollection<SubstringResult> GetResult(string oldText, string newText)
+    {
+        SetSearchMatrixSize(oldText, newText);
+        InitSearchMatrix();
+        FillSearchMatrix(oldText, newText);
+        var result = FindResult(oldText, newText);
+
+        return result;
+    }
+
+    private IReadOnlyCollection<SubstringResult> FindResult(string oldText, string newText)
+    {
+        var result = new LinkedList<SubstringResult>();
+        int count = 0;
+        for (int i = oldText.Length; i > 0;)
+        {
+            for (int j = newText.Length; j > 0;)
+            {
+                if (oldText[i - 1] == newText[j - 1])
+                {
+                    count++;
+                    i--;
+                    j--;
+                }
+                else
+                {
+                    if (count > 0)
+                    {
+                        result.AddFirst(new SubstringResult(i, j, count));
+                        count = 0;
+                    }
+                    if (_searchMatrix[i - 1, j] > _searchMatrix[i, j - 1])
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+            }
+        }
+        if (count > 0)
+        {
+            result.AddFirst(new SubstringResult(0, 0, count));
+        }
+
+        return result;
+    }
+
+    private void FillSearchMatrix(string oldText, string newText)
+    {
+        for (int i = 1; i < oldText.Length; i++)
+        {
+            for (int j = 1; j < newText.Length; j++)
+            {
+                if (oldText[i] == newText[j])
+                {
+                    _searchMatrix[i, j] = _searchMatrix[i - 1, j - 1] + 1;
+                }
+                else
+                {
+                    _searchMatrix[i, j] = Math.Max(_searchMatrix[i - 1, j], _searchMatrix[i, j - 1]);
+                }
+            }
+        }
+    }
+
+    private void InitSearchMatrix()
+    {
+        for (int i = 0; i < _searchMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < _searchMatrix.GetLength(1); j++)
+            {
+                _searchMatrix[i, j] = 0;
+            }
+        }
+    }
+
+    private void SetSearchMatrixSize(string oldText, string newText)
+    {
+        if (_searchMatrix.GetLength(0) < oldText.Length + 1 || _searchMatrix.GetLength(1) < newText.Length + 1)
+        {
+            var newLength = 2 * Math.Max(oldText.Length, newText.Length);
+            _searchMatrix = new int[newLength, newLength];
+        }
+    }
+}
